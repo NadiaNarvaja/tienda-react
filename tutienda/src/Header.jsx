ed3dc4edf31d9ef384b8038f296db14c2c324b30
NadiaNarvaja <nadia.b.narvaja>
2022-03-21 23:30:20 -0300

    Se agrego componente Header

---
+++ tutienda/src/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+            <div className="menu">
+                <h1>Bienvenido a TU TIENDA</h1>
+                
+                <nav>
+                    <a href="#">Inicio</a>
+                    <a href="#">Contacto</a>
+                    <a href="#">Productos</a>
+                </nav>
+           </div>
+    )
+
+
+}
+
+export default Header;
