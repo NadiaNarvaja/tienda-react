4fe3cefa1dc2462a9e9c69430522b07fc6fe9391
NadiaNarvaja <nadia.b.narvaja>
2022-03-22 05:11:38 -0300

    Mejora al navbar

--- tutienda/src/Header.jsx
+++ tutienda/src/Header.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './Header.css';
+import {FaShoppingCart} from 'react-icons/fa'
+
 
 const Header = () => {
     return (
             <div className="menu">
-                <h1>Bienvenido a TU TIENDA</h1>
+                <h1 className="logo">Tiendapp</h1>
                 
-                <nav>
+                <nav className="nav">
                     <a href="#">Inicio</a>
                     <a href="#">Contacto</a>
                     <a href="#">Productos</a>
+                    
                 </nav>
+                <FaShoppingCart className="carrito" size="2em"/>
+               
+              
+
            </div>
     )
 
